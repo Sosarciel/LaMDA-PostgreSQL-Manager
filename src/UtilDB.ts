2bd799d7e513c590dae8c7e7d08fd6fe20f0427c
zwa73 <ssfen2419@126.com>
2025-08-05 18:32:28 +0800

    fx

---
+++ src/UtilDB.ts
@@ -0,0 +1,22 @@
+
+
+
+
+export class UtilDB{
+    /**将js的 Date对象 转为pgsql所用的 带时区时间戳文本 */
+    static toPgTimestampZ(date = new Date()) {
+        const pad = (n: number) => String(n).padStart(2, '0');
+        const offset = -date.getTimezoneOffset();
+        const sign = offset >= 0 ? '+' : '-';
+        const hours = pad(Math.floor(Math.abs(offset) / 60));
+        const minutes = pad(Math.abs(offset) % 60);
+
+        return `${date.getFullYear()}-${pad(date.getMonth() + 1)}-${pad(date.getDate())}T`+
+            `${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(date.getSeconds())}`+
+            `${sign}${hours}:${minutes}`;
+    }
+    /**将pgsql所用的 带时区时间戳文 转为js Date对象 */
+    static fromPgTimestampZ(value:string){
+        return new Date(value);
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './Manager';
 export * from './Interface';
 export * from './Instance';
+export * from './UtilDB';
