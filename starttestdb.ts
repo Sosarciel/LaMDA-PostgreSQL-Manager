192ea92233c48d33cf386a1901977b07f665f637
zwa73 <ssfen2419@126.com>
2025-08-26 13:39:00 +0800

    fx

--- starttestdb.ts
+++ starttestdb.ts
@@ -17,6 +17,16 @@ import {DBManager} from './src';
         idleTimeoutMillis: 30000, // 30秒后关闭空闲连接
     });
     const client = await mgr.connect();
+    const listener = await mgr._pool.connect();
+    await listener.query('LISTEN operation;');
+    // 处理通知
+    listener.on('notification', async msg => {
+        const { channel, payload } = msg;
+        if(channel !== 'operation') return;
+        if(payload == undefined ) return;
+        const notify = JSON.parse(payload);
+        console.log(notify);
+    });
     //client.query(`DO $$ BEGIN RAISE EXCEPTION '测试异常输出'; END $$;`);
     try{
         await client.query(`DO $$ BEGIN RAISE NOTICE '测试Notice输出'; END $$;`);
