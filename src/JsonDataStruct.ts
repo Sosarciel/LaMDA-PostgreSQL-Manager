c7ec86e708bb8047174bcd7fe610711d703160d6
zwa73 <ssfen2419@126.com>
2025-11-07 10:12:29 +0800

    fx

--- src/JsonDataStruct.ts
+++ src/JsonDataStruct.ts
@@ -1,4 +1,5 @@
-import { DeepReadonly } from "@zwa73/js-utils";
+import { AnyFunc, DeepReadonly, JObject } from "@zwa73/js-utils";
+import { UtilFunc } from "@zwa73/utils";
 
 
 /**jsonb表标准行 */
@@ -12,3 +13,29 @@ export type DBJsonDataStruct<T> = DeepReadonly<{
         updated_at?:string;
     };
 }>;
+
+
+/**jsonb表标准实体 */
+export class DBJsonDataEntity<
+    T extends DBJsonDataStruct<any>,
+    PK extends keyof T['data']
+>{
+    constructor(protected ref:T){}
+    /**保存为JObject */
+    toJSON(): JObject {
+        return UtilFunc.deepClone(this.ref);
+    }
+    /**获取一个字段的快照, 并且在表层排除null */
+    getField(fd:keyof T['data']){
+        return this.ref.data[fd] ?? undefined;
+    }
+    /**判断数据是否有更新 */
+    checkData(data:Partial<Omit<T['data'],PK>>):T|undefined {
+        const newRow = {
+            order_id:this.ref.order_id,
+            data:Object.assign({},this.ref.data,data),
+        } as T;
+        if(UtilFunc.structEqual(this.ref.data,newRow.data)) return;
+        return newRow;
+    }
+}
